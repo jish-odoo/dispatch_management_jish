f44ac487638c0807b2d95b0fd2fe4e5e0a6eb7a6
pish-odoo <pish@odoo.com>
2024-02-07 12:47:00 +0530

    chapter 3 and 4

---
+++ est/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'est',
+    'version': '1.0',
+    'author': 'Piyush Sharma',
+    'category': 'Real Estate',
+    'depends': ['base'],  # Dependencies on other Odoo modules
+    'data': [],
+}
---
+++ est/models/__init__.py
@@ -0,0 +1,1 @@
+from . import est_property
